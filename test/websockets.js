45864a109fb284cfdbe0b59aedf5c75d1c8e7f53
nicola <notsecurity@gmail.com>
2015-09-30 10:46:13 -0400

    adding test

--- test/websockets.js
+++ test/websockets.js
@@ -3,12 +3,12 @@ var assert = require('chai').assert
 var http = require('http')
 var parallel = require('run-parallel')
 var SolidWs = require('../')
+var EventEmitter = require('events').EventEmitter
 
 describe('Solid-ws', function() {
   var server = http.createServer()
   var port = 8000
   var pubsub = SolidWs(server)
-  var client
 
   function check(msgs, uris, done) {
     parallel(msgs.map(function (msg, i) {
@@ -28,13 +28,22 @@ describe('Solid-ws', function() {
 
   before(function(done) {
     server.listen(port, function (err) {
-      if (err) done(err)
-      client = new WebSocket('http://localhost:' + port)
-      client.on('open', done)
+      done(err)
     })
+  })
+  after(function() {
+    server.close()
   })
 
   describe('sub', function() {
+    beforeEach(function(done) {
+      client = new WebSocket('http://localhost:' + port)
+      client.on('open', done)
+    })
+    afterEach(function(done) {
+      client.close()
+      done()
+    })
     it('should receive ack for any resource given', function(done) {
 
       var uris = [
@@ -78,54 +87,74 @@ describe('Solid-ws', function() {
       })
     })
   })
-  // describe('pub', function() {
-  //   describe('delete resource', function() {
-      
-  //     it('should', function(done) {
-  //       server.emit('pub '+ url)
 
-  //       client.onmessage = function(msg) {
-  //         assert.ok(msg)
-  //         assert.equal('pub ' + url)
-  //         done()
-  //       }
-  //     })
-  //   })
-  //   describe('patch resource', function() {
-      
-  //     it('should', function(done) {
-  //       server.emit('pub '+ url)
+  describe('pub', function() {
+    it('should be received by all the clients subscribed to a resource', function(done) {
 
-  //       client.onmessage = function(msg) {
-  //         assert.ok(msg)
-  //         assert.equal('pub ' + url)
-  //         done()
-  //       }
-  //     })
-  //   })
-  //   describe('put resource', function() {
-      
-  //     it('should', function(done) {
-  //       server.emit('pub '+ url)
+      var url = 'http://example.com/resource.ttl'
+      var users = [
+        'http://nicola.io#me',
+        'http://timbl.com#me', 
+        'http://deiu.io#me' ]
 
-  //       client.onmessage = function(msg) {
-  //         assert.ok(msg)
-  //         assert.equal('pub ' + url)
-  //         done()
-  //       }
-  //     })
-  //   })
-  //   describe('add resource in folder', function() {
-      
-  //     it('should', function(done) {
-  //       server.emit('pub '+ url)
+      var clients = users.map(function() {
+        return new WebSocket('http://localhost:' + port)
+      })
 
-  //       client.onmessage = function(msg) {
-  //         assert.ok(msg)
-  //         assert.equal('pub ' + url)
-  //         done()
-  //       }
-  //     })
-  //   })
-  // })
+      var pubs = []
+      var workflow = new EventEmitter()
+      workflow.on('done', function () {
+        assert.equal(pubs.length, users.length)
+        done()
+      })
+
+      connectAll(clients, function() {
+        ackAll(clients, function() {
+          pubAll(clients, function() {
+            done()
+          })
+          pubsub.publish(url)
+        })
+      })
+
+      function connectAll(clients, done) {
+        parallel(clients.map(function(client) {
+          return function (cb) {
+            client.on('open', function() {
+              client.send('sub ' + url)
+              cb()
+            })
+          }
+        }), done)
+      }
+
+      function ackAll(clients, done) {
+        parallel(clients.map(function(client) {
+          return function (cb) {
+            client.on('message', function (msg) {
+              if (msg.split(' ')[0] === 'ack') {
+                cb()
+                return;
+              }
+            })
+          }
+        }), done)
+      }
+
+      function pubAll(clients, done) {
+        parallel(clients.map(function(client) {
+          return function (cb) {
+            client.on('message', function (msg) {
+              if (msg.split(' ')[0] === 'pub') {
+                pubs.push(msg)
+                cb()
+                return;
+              }
+            })
+          }
+        }), done)
+      }
+
+    })
+  })
 })
