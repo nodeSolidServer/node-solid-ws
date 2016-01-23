a5cd6cbef19605d79bda77193fad7f863c3a4895
nicola <notsecurity@gmail.com>
2016-01-23 16:45:09 -0500

    fixing reference to path

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
 var debug = require('debug')('ldnode:ws-app')
 var SolidWs = require('./lib/server')
+var path = require('path')
 
 module.exports = function attachToServer (server, app, opts) {
   var solidWs = new SolidWs(server, opts)
 
   if (app) {
-    console.log("setting up the app")
+    console.log('setting up the app')
 
     app.post('/*', function (req, res, next) {
-      debug("pub " + req.originalUrl  + ' after post')
+      debug('pub ' + req.originalUrl + ' after post')
       solidWs.publish(req.originalUrl)
       solidWs.publish(path.basename(req.originalUrl))
       next()
     })
     app.patch('/*', function (req, res, next) {
-      debug("pub " + req.originalUrl + ' after patch')
+      debug('pub ' + req.originalUrl + ' after patch')
       solidWs.publish(req.originalUrl)
       console.log(solidWs.store)
       next()
     })
     app.put('/*', function (req, res, next) {
-      debug("pub " + req.originalUrl + ' after put')
+      debug('pub ' + req.originalUrl + ' after put')
       solidWs.publish(req.originalUrl)
       next()
     })
     app.delete('/*', function (req, res, next) {
-      debug("pub " + req.originalUrl  + ' after delete')
+      debug('pub ' + req.originalUrl + ' after delete')
       solidWs.publish(req.originalUrl)
       solidWs.publish(path.basename(req.originalUrl))
       next()
