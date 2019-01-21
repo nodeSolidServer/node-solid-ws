a0498ddf99ff21cb6bd30c50b509277eff244e75
Michiel de Jong <michiel@unhosted.org>
2019-01-21 15:20:05 +0100

    only publish if resource is public

--- index.js
+++ index.js
@@ -4,15 +4,20 @@ var path = require('path')
 
 module.exports = function attachToServer (server, app, opts) {
   var solidWs = new SolidWs(server, opts)
-
   function publish (req, res, next) {
-    debug('pub ' + req.originalUrl + ' after edit')
-    solidWs.publish(req.originalUrl)
-    var parent = path.dirname(req.originalUrl) + path.sep
-    if (parent !== req.originalUrl) {
-      solidWs.publish(parent)
-    }
-    next()
+    req.acl.can(null, 'Read').then(isPublic => {
+      if (isPublic) {
+        debug('pub ' + req.originalUrl + ' after edit')
+        solidWs.publish(req.originalUrl)
+        var parent = path.dirname(req.originalUrl) + path.sep
+        if (parent !== req.originalUrl) {
+          solidWs.publish(parent)
+        }
+      } else {
+        debug('do not pub ' + req.originalUrl + ' after edit')
+      }
+      next()
+    })
   }
 
   if (app) {
