aaedba6fb873c1c6fc2868b316db476d04b78640
Michiel de Jong <michiel@unhosted.org>
2019-01-21 14:06:06 +0100

    [refactor] reuse publish function

--- index.js
+++ index.js
@@ -5,44 +5,21 @@ var path = require('path')
 module.exports = function attachToServer (server, app, opts) {
   var solidWs = new SolidWs(server, opts)
 
-  if (app) {
+  function publish (req, res, next) {
+    debug('pub ' + req.originalUrl + ' after edit')
+    solidWs.publish(req.originalUrl)
+    var parent = path.dirname(req.originalUrl) + path.sep
+    if (parent !== req.originalUrl) {
+      solidWs.publish(parent)
+    }
+    next()
+  }
 
-    app.post('/*', function (req, res, next) {
-      debug('pub ' + req.originalUrl + ' after post')
-      solidWs.publish(req.originalUrl)
-      var parent = path.dirname(req.originalUrl) + path.sep
-      if (parent !== req.originalUrl) {
-        solidWs.publish(parent)
-      }
-      next()
-    })
-    app.patch('/*', function (req, res, next) {
-      debug('pub ' + req.originalUrl + ' after patch')
-      solidWs.publish(req.originalUrl)
-      var parent = path.dirname(req.originalUrl) + path.sep
-      if (parent !== req.originalUrl) {
-        solidWs.publish(parent)
-      }
-      next()
-    })
-    app.put('/*', function (req, res, next) {
-      debug('pub ' + req.originalUrl + ' after put')
-      solidWs.publish(req.originalUrl)
-      var parent = path.dirname(req.originalUrl) + path.sep
-      if (parent !== req.originalUrl) {
-        solidWs.publish(parent)
-      }
-      next()
-    })
-    app.delete('/*', function (req, res, next) {
-      debug('pub ' + req.originalUrl + ' after delete')
-      solidWs.publish(req.originalUrl)
-      var parent = path.dirname(req.originalUrl) + path.sep
-      if (parent !== req.originalUrl) {
-        solidWs.publish(parent)
-      }
-      next()
-    })
+  if (app) {
+    app.post('/*', publish)
+    app.patch('/*', publish)
+    app.put('/*', publish)
+    app.delete('/*', publish)
   }
 
   return solidWs
